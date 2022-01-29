3ef968c7a0ce5e9cab6e9e0e2716efebb05a4b65
Omar AbuRish <191020@ppu.edu.ps>
2022-01-29 11:45:17 +0200

    Converted Main page HTML to React

---
+++ currency-exchange-ui/src/App.js
@@ -0,0 +1,14 @@
+
+import Header from './components/header/header-component';
+import Home from './components/home/home-component';
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+      <Home></Home>
+    </>
+  );
+}
+
+export default App;
---
+++ currency-exchange-ui/src/components/home/home-component.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './home-component.css'
+function Home() {
+  return (<section id='home-section'>
+      <div class="black-overlay">
+        <div class="home-content center">
+            <h1>The World's Trusted Currency Authority</h1>
+            <p>Check exchange rates, send money internationally, and free currency tools</p>
+        </div>
+      </div>
+   
+
+            
+  </section>);
+}
+
+export default Home;
